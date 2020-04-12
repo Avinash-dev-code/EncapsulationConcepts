15e4ceb6bbaf06740ea91ff4e62935bda4f95891
AvinashDev <55042628+Avinash-dev-code@users.noreply.github.com>
2020-04-12 20:43:59 +0530

    program added..

---
+++ Voters/Abc.java
@@ -0,0 +1,35 @@
+
+
+public class Abc {
+
+public static void main(String[] args) {
+    String abc="Abc";
+   
+    
+     //    System.out.println(abc.charAt(0));
+     
+      for(int i=1;i<=1;i++)
+      {
+
+           System.out.println(abc.substring(0, 1));
+            for(int j=2;j<=2;j++)
+            {
+              System.out.println(abc.substring(0, 2));
+            }
+            for(int k=2;k<=2;k++)
+            {
+              System.out.println(abc.substring(0, 3));
+            }
+           
+
+
+        
+      }
+
+         
+     
+
+
+}
+
+}
